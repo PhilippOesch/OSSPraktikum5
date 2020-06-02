98227327273d7749900c126d7e6377469c5ee64f
Philipp Oeschger <philipp.oeschger@hs-furtwangen.de>
2020-06-02 23:31:11 +0200

    aufgabe 10

--- src/main/java/de/hfu/MeineHauptklasse.java
+++ src/main/java/de/hfu/MeineHauptklasse.java
@@ -2,12 +2,35 @@ package de.hfu;
 
 import java.util.Scanner;
 
-public class MeineHauptklasse {
+/** 
+ * Das ist meine Mainklasse
+ * 
+ * @author Philipp Oeschger
+ * @version 1.0
+ **/
 
+public class MeineHauptklasse {
+	
+	/**
+	 *  Hier ist meine Main-Methode 
+	 *  
+	 * @param args Kommandozeilenparameter
+	 **/
 	public static void main(String[] args) {
 		
+		/**
+		 * Hier wird der Scanner initialisiert 
+		 **/
 		Scanner eingabewert = new Scanner(System.in);
+		
+		/**
+		 *  Auslesen der Scanner Eingabe 
+		 **/
 		String value= eingabewert.nextLine();
+		
+		/**
+		 *  Ausgabe in Großbuchstaben
+		 **/
 		System.out.println(value.toUpperCase());
 
 	}
